b6c3f913735c08bb89ec12be7618b02c6d9f6046
Noman <mdnoman70499@gmail.com>
2023-10-10 12:16:34 +0530

    sqrt question added

---
+++ 01_Easy/sqrt.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int sqrrt(int n){
+    return sqrt(n);
+}
+int main()
+{
+    int a;
+    cout << sqrrt(4);
+
+    return 0;
+}
---
+++ 03_Hard/chck.js
@@ -0,0 +1,8 @@
+class program 
+{
+    static void Main[] args;
+    console.writeline("Hello")
+        ;
+    console.writeline("students")
+    consol.readline();
+}
